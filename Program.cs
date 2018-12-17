281a9072805cc7c88298385d89a2e0fd5cc0e9dd
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 14:45:52 +0100

    Exercice 02 complété !

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = 30;
+
+            
+
+            int resultat = (x + y) * z;
+
+
+            Console.WriteLine(x + "+" + y + "x" + z + "=" + resultat);
+
+
+            Console.ReadKey();
+        }
+    }
+}
